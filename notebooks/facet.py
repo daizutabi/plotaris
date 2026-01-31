7de044ad4f334e1059828c158ea6582b52c75043
Daizu <daizutabi@gmail.com>
2026-01-31 06:55:07 +0000

    refactor(facet): Update data preparation and enhance FacetGrid title formatting

--- notebooks/facet.py
+++ notebooks/facet.py
@@ -4,9 +4,12 @@ __generated_with = "0.19.7"
 app = marimo.App(width="medium")
 
 with app.setup:
+    import matplotlib.pyplot as plt
     import polars as pl
 
     from plotaris import FacetGrid
+
+    plt.style.use(["default", "matplotlibrc"])
 
 
 @app.cell
@@ -14,7 +17,8 @@ def _():
     data = pl.DataFrame(
         {
             "a": [1, 1, 1, 2, 2, 2],
-            "b": [3, 4, 5, 3, 4, 5],
+            # "b": [3, 4, 5, 3, 4, 5],
+            "b": [3, 3, 4, 4, 5, 5],
             "x": range(6),
             "y": range(10, 16),
         },
@@ -24,7 +28,8 @@ def _():
 
 @app.cell
 def _(data):
-    FacetGrid(data, row="a", col="b", figsize=(4, 2))
+    grid = FacetGrid(data, row="a", col="b", figsize=(4, 2))
+    grid.set_titles({"b": "{:.1f}"}, a="A(m)", margin_titles=True)
     return
 
 
